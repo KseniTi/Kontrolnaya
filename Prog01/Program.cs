4a29a0f546358915e05d5a012c8cc87d258afba1
KseniTi <lioness2006@bk.ru>
2023-01-03 20:45:58 +0300

    Программа для решения задачи

---
+++ Prog01/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void Zadacha()
+{
+    string[] words = { "hello", "12348", "45", "cat" };
+    int length = words.Length;
+    string[] wordsNew = new string[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        if (words[i].Length <= 3)
+        {
+            wordsNew[i] = words[i];
+            Console.Write(wordsNew[i] + " ");
+        }
+    }
+}
+Zadacha();
